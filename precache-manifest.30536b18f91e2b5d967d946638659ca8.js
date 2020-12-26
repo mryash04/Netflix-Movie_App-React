fec991a1a7fdff79ab779936bf718ea4e2541019
mryash04 <yash.04may@gmail.com>
2020-12-26 18:08:52 +0530

    Updates

---
+++ precache-manifest.30536b18f91e2b5d967d946638659ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60029690e3825b5ccb6172a7877edb3",
+    "url": "/Netflix-Movie_App-React/index.html"
+  },
+  {
+    "revision": "d47e4fe7b985bbfab0c5",
+    "url": "/Netflix-Movie_App-React/static/css/main.a25c4036.chunk.css"
+  },
+  {
+    "revision": "a746ceffe2795df2eced",
+    "url": "/Netflix-Movie_App-React/static/js/2.918db438.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Netflix-Movie_App-React/static/js/2.918db438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d47e4fe7b985bbfab0c5",
+    "url": "/Netflix-Movie_App-React/static/js/main.5ba193f4.chunk.js"
+  },
+  {
+    "revision": "e5a46b10016b398ed9b9",
+    "url": "/Netflix-Movie_App-React/static/js/runtime-main.ae377ed0.js"
+  }
+]);
